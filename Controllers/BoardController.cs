25dbc60def9d68eb201c6ee9db5031e59857cb29
Arancreat <arancreat@gmail.com>
2024-01-09 17:44:40 +0900

    Add basic controllers

---
+++ Controllers/BoardController.cs
@@ -0,0 +1,30 @@
+﻿using API.NETBoard.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.NETBoard.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardController(IBoardService boardService) : ControllerBase
+    {
+        private readonly IBoardService _boardService = boardService;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                return Ok(await _boardService.GetAll());
+            } 
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    err = ex.InnerException?.Message ?? ex.Message,
+                    stackTrace = ex.StackTrace
+                });
+            }
+        }
+    }
+}
---
+++ Controllers/PostController.cs
@@ -0,0 +1,46 @@
+﻿using API.NETBoard.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.NETBoard.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController(IPostService postService) : ControllerBase
+    {
+        private readonly IPostService _postService = postService;
+
+        [HttpGet("/get-threads")]
+        public async Task<IActionResult> GetThreads()
+        {
+            try
+            {
+                return Ok(await _postService.GetThreads());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    err = ex.InnerException?.Message ?? ex.Message,
+                    stackTrace = ex.StackTrace
+                });
+            }
+        }
+
+        [HttpGet("/get-replies/{threadId}")] 
+        public async Task<IActionResult> GetReplies(long threadId)
+        {
+            try
+            {
+                return Ok(await _postService.GetReplies(threadId));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    err = ex.InnerException?.Message ?? ex.Message,
+                    stackTrace = ex.StackTrace
+                });
+            }
+        }
+    }
+}
