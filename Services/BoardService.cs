8cf6f84e5a1a7cf92faeb1d8f0aaac68faf1a6c4
Arancreat <arancreat@gmail.com>
2024-01-09 17:44:28 +0900

    Add service classes

---
+++ Services/BoardService.cs
@@ -0,0 +1,21 @@
+﻿using API.NETBoard.Database;
+using API.NETBoard.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.NETBoard.Services
+{
+    public interface IBoardService
+    {
+        public Task<List<Board>> GetAll();
+    }
+
+    public class BoardService(DataContext dataContext) : IBoardService
+    {
+        private readonly DataContext _dataContext = dataContext;
+
+        public async Task<List<Board>> GetAll()
+        {
+            return await _dataContext.Boards.ToListAsync();
+        }
+    }
+}
---
+++ Services/PostService.cs
@@ -0,0 +1,28 @@
+﻿using API.NETBoard.Database;
+using API.NETBoard.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.NETBoard.Services
+{
+    public interface IPostService
+    {
+        public Task<List<Post>> GetThreads();
+
+        public Task<List<Post>> GetReplies(long threadId);
+    }
+
+    public class PostService(DataContext dataContext) : IPostService
+    {
+        private readonly DataContext _dataContext = dataContext;
+        
+        public async Task<List<Post>> GetThreads()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Post>> GetReplies(long threadId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
