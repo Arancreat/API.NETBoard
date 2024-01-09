0a0a6973742c0a0da5bbde735e24015fed6151f2
Arancreat <arancreat@gmail.com>
2024-01-09 17:44:15 +0900

    Add Context

---
+++ Database/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using API.NETBoard.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.NETBoard.Database
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Board> Boards { get; set; }
+
+        public DbSet<Post> Messages { get; set; }
+    }
+}
