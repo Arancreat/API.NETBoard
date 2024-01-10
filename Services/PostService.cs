e18dbe1c2b5ea30169c180cc1317384659849991
Arancreat <arancreat@gmail.com>
2024-01-10 17:09:14 +0900

    PostService - implemented basic get

--- Services/PostService.cs
+++ Services/PostService.cs
@@ -1,28 +1,31 @@
 ﻿using API.NETBoard.Database;
 using API.NETBoard.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.NETBoard.Services
 {
     public interface IPostService
     {
-        public Task<List<Post>> GetThreads();
+        public Task<List<Post>> GetAllOPs();
 
-        public Task<List<Post>> GetReplies(long threadId);
+        public Task<List<Post>> GetThread(long threadId);
     }
 
     public class PostService(DataContext dataContext) : IPostService
     {
         private readonly DataContext _dataContext = dataContext;
         
-        public async Task<List<Post>> GetThreads()
+        public async Task<List<Post>> GetAllOPs()
         {
-            throw new NotImplementedException();
+            return await _dataContext.Posts.Where(p => p.ParentId == null).ToListAsync();
         }
 
-        public async Task<List<Post>> GetReplies(long threadId)
+        public async Task<List<Post>> GetThread(long threadId)
         {
-            throw new NotImplementedException();
+            return await _dataContext.Posts
+                .Where(p => p.Id == threadId || p.ParentId == threadId)
+                .OrderBy(p => p.Id).ToListAsync();
         }
     }
 }
